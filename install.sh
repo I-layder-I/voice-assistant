0a4661d71ca49a36576dd0e7f81bf562e1ccec9d
I-layder-I <evgeniy.izmalkov10@gmail.com>
2026-06-15 04:27:42 +0300

    Update inst & uninst

--- install.sh
+++ install.sh
@@ -2,34 +2,28 @@
 set -euo pipefail
 
 TMP=$(mktemp -d)
+URL="https://github.com/I-layder-I/voice-assistant/releases/latest/download/voice-assistant.tar.gz"
 
-git clone https://github.com/I-layder-I/voice-assistant "$TMP"
 echo
-
 echo "Wellcome to Voice-assistant!"
 echo
+
+echo "Downloading latest release..."
+curl -fL "$URL" -o "$TMP/release.tar.gz"
+tar -xzf "$TMP/release.tar.gz" -C "$TMP"
 
 echo "Copying binary..."
 sudo install -Dm755 "$TMP/voice-assistant" \
 /usr/local/bin/voice-assistant
-echo "Copying libs..."
-sudo install -Dm644 "$TMP/libvosk.so" \
-/usr/local/lib/libvosk.so
-echo "Copying headers..."
-sudo install -Dm644 "$TMP/vosk_api.h" \
-/usr/local/include/vosk_api.h
 
 echo "Copying model..."
-mkdir -p ~/.local/share/voice-assistant
+install -d ~/.local/share/voice-assistant
 cp -r "$TMP/model" \
 ~/.local/share/voice-assistant/
 echo "Copying commands..."
-mkdir -p ~/.config/voice-assistant
+install -d ~/.config/voice-assistant
 cp -r "$TMP/commands" \
 ~/.config/voice-assistant/
-
-echo "Updating lib cache..."
-sudo ldconfig
 
 rm -rf "$TMP"
 
--- uninstall.sh
+++ uninstall.sh
@@ -3,12 +3,5 @@ set -euo pipefail
 
 echo "Removing binary..."
 sudo rm -f /usr/local/bin/voice-assistant
-echo "Removing libs..."
-sudo rm -f /usr/local/lib/libvosk.so
-echo "Removing headers..."
-sudo rm -f /usr/local/include/vosk_api.h
 
-echo "Updating lib cache..."
-sudo ldconfig
-
-echo "Done"
+echo "Done."
